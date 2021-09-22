2c319a09a4c9c982b0012fc83ed320d6da00ce19
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2021-09-22 10:17:26 -0300

    Create main.cpp

---
+++ Algoritmos e Lógica de Programação/LE01/Ex03/C++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int num1 = 0, num2 = 0, mult = 0;
+	
+	cout << "Este programa recebe dois valores e exibe a multiplicaçao\n";
+	cout << "Insira um numero inteiro: ";
+	cin >> num1;
+	cout << "Insira outro numero inteiro: ";
+	cin >> num2;
+	
+	mult = num1 * num2;
+	
+	cout << "O resultado da multiplicaçao eh: " << mult;
+	
+	return 0;
+}
