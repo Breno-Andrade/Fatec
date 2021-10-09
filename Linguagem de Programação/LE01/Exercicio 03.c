6d5d3c2d20e024436bb9265085ba98114fd02af1
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2021-10-08 22:27:15 -0300

    Create Exercicio 03.c

---
+++ Linguagem de Programação/LE01/Exercicio 03.c
@@ -0,0 +1,12 @@
+// Converte numero Hexadecimal para Decimal
+#include <stdio.h>
+
+int main(void) {
+    int n = 0;
+
+    printf("Digite o numero Hexadecimal: ");    // Entrada de Dados
+    scanf("%x", &n);
+
+    printf("Numero em decimal: %d", n);         // Saida de dados.
+    return 0;
+}
