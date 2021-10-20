9e4041f759d7ca46064742789f2a2a9a688822ac
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2021-10-19 22:55:26 -0300

    Create Exercício 11.c

---
+++ Linguagem de Programação/LE02/Exercício 11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    float x, y;
+    char o;
+
+    printf("Digite a expressao: ");
+    scanf("%f %c %f", &x, &o, &y);
+
+    switch (o){
+        case '+': printf("Valor = %.2f\n", x + y); break;
+        case '-': printf("Valor = %.2f\n", x - y); break;
+        case '*': printf("Valor = %.2f\n", x * y); break;
+        case 'x': printf("Valor = %.2f\n", x * y); break;
+        case '/': if (y == 0){
+            printf("O divisor deve ser maior que 0!"); break;
+        } else {
+            printf("Valor = %.2f\n", x / y); break;
+        }
+        case ':': if (y == 0){
+            printf("O divisor deve ser maior que 0!"); break;
+        } else {
+            printf("Valor = %.2f\n", x / y); break;
+        }
+        default: printf("Operador invalido: %c\n", o);
+    }
+    return 0;
+}
